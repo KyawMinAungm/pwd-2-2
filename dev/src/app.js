71635d07cac362dfa4a765d69d4443bae440e399
Ei Maung <eimg@fairwayweb.com>
2025-10-25 13:06:10 +0630

    initial commit

---
+++ dev/src/app.js
@@ -0,0 +1,15 @@
+function Element(props) {
+	return <ul>
+        <li>Item One</li>
+        <li>Item Two</li>
+        <li>Item Three</li>
+        <li>Item Three</li>
+        <li>Item Three</li>
+        <li>Item Three</li>
+    </ul>;
+}
+
+ReactDOM.render(
+	<Element content="A React Component" />,
+	document.getElementById("app")
+);
---
+++ hello-react/src/Item.jsx
@@ -0,0 +1,19 @@
+import { ListItem, ListItemText, IconButton } from "@mui/material";
+
+import {
+    SquareOutlined as CheckIcon,
+    Check as UndoIcon,
+    Delete as DeleteIcon
+} from "@mui/icons-material";
+
+export default function Item({ item, del, toggle }) {
+    return <ListItem>
+        <IconButton onClick={() => toggle(item.id)}>
+            {item.done ? <UndoIcon color="success" /> : <CheckIcon />}
+        </IconButton>
+        <ListItemText primary={item.name} />
+        <IconButton onClick={() => del(item.id)}>
+            <DeleteIcon />
+        </IconButton>
+    </ListItem>;
+}
