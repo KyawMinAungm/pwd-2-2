924144c917c45278e29db4e36348739c513bc106
Ei Maung <eimg@fairwayweb.com>
2025-11-15 14:51:58 +0630

    social api

---
+++ social-api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Social API", status: "Running..." });
+});
+
+app.listen(8800, () => {
+    console.log("Social API running at port 8800...");
+});
